ca305a21353289e6bdd0ece6fdece945bdbb6723
F. Dan O'Neill <fdfoneill@gmail.com>
2026-03-04 18:31:48 -0500

    Implement supply consumption math and display on player dashboard

--- forwantofanail/mechanics/__init__.py
+++ forwantofanail/mechanics/__init__.py
@@ -1,5 +1,6 @@
 from .time import GameTime, Watch, advance_time
 from .movement import calculate_move_watches, MoveResult, list_valid_destinations, move_army
+from .supply import consume_supply_for_all_armies, daily_supply_consumption, supply_capacity, supply_stats
 
 __all__ = [
     "GameTime",
@@ -9,4 +10,8 @@ __all__ = [
     "move_army",
     "list_valid_destinations",
     "calculate_move_watches",
+    "consume_supply_for_all_armies",
+    "daily_supply_consumption",
+    "supply_capacity",
+    "supply_stats",
 ]
